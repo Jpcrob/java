87448bcf1ca37ad283468ab8382784f9f0e39775
Jpcrob <jprobasto@gmail.com>
2023-12-12 06:23:53 +0800

    Update Largest.java

--- src/test/pack/Largest.java
+++ src/test/pack/Largest.java
@@ -11,7 +11,13 @@ public class Largest {
         double num2 = scanner.nextDouble();
         System.out.println("Enter the third number:");
         double num3 = scanner.nextDouble();
+        
+        if (num1 == num2 && num1 == num3) {
+            System.out.println("All numbers are equal.");
+        }
+        else {
         double largest = (num1 >= num2 && num1 >= num3) ? num1 : (num2 >= num1 && num2 >= num3) ? num2 : num3;
         System.out.println("The largest number is: " + largest);
+        }
     }
 }
